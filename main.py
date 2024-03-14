941abaf724ffe1bfc05bf529895d8f8c3184d2da
Karol Niekrasz <karolniekrasz@gmail.com>
2024-03-14 08:33:27 +0100

    all basic functions created

--- main.py
+++ main.py
@@ -13,34 +13,53 @@ def readTrainingFile(file_name):
     return df.values.tolist()
 
 def readTestFile(file_name):
+    df = pd.read_csv(file_name, delimiter=';')
 
     df = pd.read_csv(file_name, delimiter=';')
 
-    return df.iloc[:, :-1].values.tolist()
+    return df.iloc[:,:-1].values.tolist()
 
-list=readTrainingFile("C:\\Users\\48509\\Desktop\\iris.csv")
-
+trainingList=readTrainingFile("C:\\Users\\48509\\Desktop\\iris.csv")
+testList=readTestFile("C:\\Users\\48509\Desktop\\TestFile.csv")
 ##trainingFilePath = input("Give path to training file :")
 
 ##testFilePath = input("Give path to test file :")
 
-k = input("Give k")
 
 ##vectors = readTrainingFile(trainingFilePath)
 
 ##Creating map of vectors
-
-mapa = {}
-for listOfList in list:
-        key = listOfList[-1]
-        value = listOfList[:-1]
-        if key in mapa.keys():
-            mapa[key]+=value
-        else:
-            mapa[key] = [value]
+def create_map_ofTraingFIle(trainingList):
+    mapa = {}
+    for listOfList in trainingList:
+            key = listOfList[-1]
+            value = listOfList[:-1]
+            if key in mapa.keys():
+                mapa[key].append(value)
+            else:
+                mapa[key] = [value]
+    return mapa
 
 
 ##vectorsToClassified = readTestFile(testFilePath)
+exampleVec=[1,3,4,4]
 
-closestVec = [];
 
+
+def k_nearest_neighbors(mapa, exampleVec, k):
+    closestVec = []
+    for key, values in mapa.items():
+        for value in values:
+            distance = euclidesLenght(exampleVec, value)
+            closestVec.append((key, distance))
+
+    closestVec.sort(key=lambda x: x[1])
+
+    return closestVec[:k]
+
+print(testList)
+for testingData in testList:
+    k_nearest_neighbors(mapa,testingData,3)
+
+
+    ##TODO zmienić funkcje do czytania tego gówna
